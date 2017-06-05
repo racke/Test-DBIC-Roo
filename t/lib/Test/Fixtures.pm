f55bb02137558c66358de0deae065a6bd96b099a
Stefan Hornburg (Racke) <racke@linuxia.de>
2017-06-05 12:37:48 +0200

    Add test for entry in jobs table.

--- t/lib/Test/Fixtures.pm
+++ t/lib/Test/Fixtures.pm
@@ -3,4 +3,12 @@ package Test::Fixtures;
 use Test::Exception;
 use Test::Roo::Role;
 
+test 'jobs' => sub {
+    my $self = shift;
+    my $schema = $self->schema;
+
+    cmp_ok ( $self->jobs->find ( { name => 'dancer' } )->description,
+             'eq', 'Perl Dancer programmer', "job description for dancer" );
+};
+
 1;
