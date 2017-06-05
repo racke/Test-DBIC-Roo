06ffcbd85850f28cef115e38b313ebbf09ebc927
Stefan Hornburg (Racke) <racke@linuxia.de>
2017-06-05 12:45:38 +0200

    Add abstract and description to main module.

--- lib/Test/DBIC/Roo.pm
+++ lib/Test/DBIC/Roo.pm
@@ -4,9 +4,13 @@ our $VERSION = '0.133';
 
 =encoding utf8
 
-=head1 NAME
+=head1 Name
 
-Test::DBI::Roo
+Test::DBIC::Roo - Role based testing for DBIx::Class schemas.
+
+=head1 Description
+
+Tests for L<DBIx::Class> schemas using role based testing provided by L<Test::Roo>.
 
 =cut
 
